0331f3dd869e4c1c578b7e3ba3185993dbc41879
Vitaliy <vitaliysidorov@yandex.ru>
2023-02-11 22:21:54 +0500

    initial Commit

---
+++ 064_Task/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int ReadNumber() // Метод проверки соответствия вводимого числа условиям задачи
+{
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+        {
+            return number;
+        }
+        else Console.WriteLine("Не удалось распознать требуемое число, попробуйте еще раз.");
+    }
+}
+
+int PrintNatural(int number)
+{
+    if (number == 0)
+    {
+        return -1;
+    }
+    Console.Write("{0} ", number);
+    return PrintNatural(number - 1);
+}
+
+Console.Clear();
+Console.WriteLine("Программа вывода всех натуральных чисел в промежутке от n до 1.");
+Console.Write("Введите число n: ");
+int number = ReadNumber();
+PrintNatural(number);
+Console.WriteLine();
---
+++ 066_Task/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 66: Задайте значения m и n. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от m до n.
+// m = 1; n = 15 -> 120
+// m = 4; n = 8. -> 30
+
+int ReadNumber() // Метод проверки соответствия вводимого числа условиям задачи
+{
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+        {
+            return number;
+        }
+        else Console.WriteLine("Не удалось распознать натуральное число, попробуйте еще раз.");
+    }
+}
+
+int SumBetweenMN(int m, int n) // Рекурсивный метод нахождения суммы натуральных элементов в промежутке [m, n]
+{
+    if (m == n)
+    {
+        return m;
+    }    
+    else if (m < n) // Проверка чисел на больше-меньше для определения направления суммирования
+    {
+        return n + SumBetweenMN(m, n - 1);
+    }
+    else 
+    {
+        return n + SumBetweenMN(m, n + 1);
+    }
+
+}
+
+Console.Clear();
+Console.WriteLine("Программа нахождения суммы натуральных элементов в промежутке [m, n].");
+Console.Write("Введите число m: ");
+int m = ReadNumber();
+Console.Write("Введите число n: ");
+int n = ReadNumber();
+Console.WriteLine($"Сумма натуральных элементов в промежутке [{m}, {n}] равна {SumBetweenMN(m, n)}.");
